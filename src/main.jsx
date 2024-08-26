5f97eba5cf5c953e5b9de81894446b1c1bfaccc6
Jash2606 <jash.23bcs10163@sst.scaler.com>
2024-08-26 06:58:51 +0530

    Created Login template...

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter , RouterProvider  } from 'react-router-dom'
+import Login from './Login/login.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  { 
+    path: "/",
+    element: <App />,
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
