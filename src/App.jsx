2209594a37de4c3e9d38bc2171b06d4115f89dfe
Jash2606 <jash.23bcs10163@sst.scaler.com>
2024-08-26 07:37:29 +0530

    Add Dashboard Header and Google Login Auth..

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import TopOnebox from './components/topOneBox';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const selectedUrl = useSelector((state) => state.counter.url);
+  const navigate = useNavigate();
+  
+  const queryParams = new URLSearchParams(window.location.search);
+  const token = queryParams.get("token");
+  const localStorageToken = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (!token && !localStorageToken) {
+      navigate("/login");
+    } else {
+      if (token) {
+        localStorage.setItem("token", `Bearer ${token}`);
+      }
+    }
+  }, [token, localStorageToken, navigate]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <TopOnebox />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter , RouterProvider  } from 'react-router-dom'
 import Login from './Login/login.jsx'
+import { Provider } from 'react-redux'
+import store from "./app/store";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +19,8 @@ const router = createBrowserRouter([
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
